e5e0801e4ff2a36ad5abd337912f2aef724ca9b7
Sofiane <sofiane.mahmoudi@ynov.com>
2021-09-23 16:10:20 +0200

    poison

--- poisonpot.go
+++ poisonpot.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-
+type personnage struct {
+	HP int
+}
 
 func (p *personnage) PoisonPot() {
-	for i := 1 ; i <= 3; i++ {
-		p.HP - 10
-		fmt.Println(p.HP)
+	for i := 1; i <= 3; i++ {
+		p.HP = p.HP - 10
+		fmt.Println("salut")
+		time.Sleep(1 * time.Second)
 	}
-
-	time.Sleep(1 * time.Second)
-
 }
